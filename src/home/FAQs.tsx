6d6f9dffe079e1d720d3bb1a6bde9b9b6dfb3fd9
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-09-22 00:04:06 +0100

    FAQs

--- src/home/FAQs.tsx
+++ src/home/FAQs.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import faq from "../assets/the_big_idea .png"
 const FAQs = () => {
   return (
-    <div className="space-y-4">
-    <details
-      className="group border-s-4 border-[#D434FE] bg-[#150E28] p-6 [&_summary::-webkit-details-marker]:hidden"
+    <div className="space-y-4 mt-[30px] flex flex-col justify-center items-center w-[100%]">
+        <div className='text-left w-[90%]'>
+        <h5 className=" text-4xl font-extrabold leading-none text-white mb-[40px] mt-[30px]">
+              Introduction to getlinked
+
+                <br className="hidden md:block" />
+                
+                <span className="inline-block text-deep-purple-accent-400 text-[#D434FE]">
+                tech Hackathon 1.0
+                </span>
+              </h5>
+        <p className='text-white'>We got answers to the questions that you might
+want to ask about getlinked Hackathon 1.0</p>
+        </div>
+   <div className='w-[90%] flex justify-between flex-wrap'>
+  <div className='w-[55%]'>
+  <details
+      className="group border-b-2 border-[#D434FE] bg-[#150E28] p-6 [&_summary::-webkit-details-marker]:hidden"
       open
     >
       <summary className="flex cursor-pointer items-center justify-between gap-1.5">
@@ -12,7 +27,7 @@ const FAQs = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing?
         </h2>
   
-        <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+        <span className="shrink-0 rounded-full bg-transparent text-[#D434FE]  p-1.5 sm:p-3">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
@@ -36,7 +51,7 @@ const FAQs = () => {
       </p>
     </details>
     <details
-      className="group border-s-4 border-[#D434FE] bg-[#150E28] p-6 [&_summary::-webkit-details-marker]:hidden"
+      className="group border-b-2 border-[#D434FE] bg-[#150E28] p-6 [&_summary::-webkit-details-marker]:hidden"
       open
     >
       <summary className="flex cursor-pointer items-center justify-between gap-1.5">
@@ -44,7 +59,7 @@ const FAQs = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing?
         </h2>
   
-        <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+        <span className="shrink-0 rounded-full bg-transparent text-[#D434FE]  p-1.5 sm:p-3">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
@@ -68,7 +83,7 @@ const FAQs = () => {
       </p>
     </details>
     <details
-      className="group border-s-4 border-[#D434FE] bg-[#150E28] p-6 [&_summary::-webkit-details-marker]:hidden"
+      className="group border-b-2 border-[#D434FE] bg-[#150E28] p-6 [&_summary::-webkit-details-marker]:hidden"
       open
     >
       <summary className="flex cursor-pointer items-center justify-between gap-1.5">
@@ -76,7 +91,7 @@ const FAQs = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing?
         </h2>
   
-        <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+        <span className="shrink-0 rounded-full bg-transparent text-[#D434FE]  p-1.5 sm:p-3">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
@@ -100,7 +115,7 @@ const FAQs = () => {
       </p>
     </details>
     <details
-      className="group border-s-4 border-[#D434FE] bg-[#150E28] p-6 [&_summary::-webkit-details-marker]:hidden"
+      className="group border-b-2 border-[#D434FE] bg-[#150E28] p-6 [&_summary::-webkit-details-marker]:hidden"
       open
     >
       <summary className="flex cursor-pointer items-center justify-between gap-1.5">
@@ -108,7 +123,7 @@ const FAQs = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing?
         </h2>
   
-        <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+        <span className="shrink-0 rounded-full bg-transparent text-[#D434FE]  p-1.5 sm:p-3">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
@@ -131,6 +146,45 @@ const FAQs = () => {
         earum similique!
       </p>
     </details>
+    <details
+      className="group border-b-2 border-[#D434FE] bg-[#150E28] p-6 [&_summary::-webkit-details-marker]:hidden"
+      open
+    >
+      <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+        <h2 className="text-lg font-medium text-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing?
+        </h2>
+  
+        <span className="shrink-0 rounded-full bg-transparent text-[#D434FE]  p-1.5 sm:p-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+      </summary>
+  
+      <p className="mt-4 leading-relaxed text-white">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+        molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+        voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+        earum similique!
+      </p>
+    </details>
+  </div>
+
+  <div>
+  <img src={faq} alt="" />
+  </div>
+   </div>
+  
   
   
   </div>
