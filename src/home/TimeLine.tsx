d80963f6e6deb28218a742797754c8ee224736f5
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-09-22 09:49:39 +0100

    timeline

--- src/home/TimeLine.tsx
+++ src/home/TimeLine.tsx
@@ -5,42 +5,130 @@ const TimeLine = () => {
 
    
     
-        const [steps, setStep] = useState({
-            stepsItems: ["Profile", "Contact", "Identity", "Passport"],
-            currentStep: 2
-        })
-    
+
         return (
-            <div className="max-w-2xl mx-auto px-4 md:px-0">
-                <ul aria-label="Steps" className="items-center text-gray-600 font-medium md:flex">
-                    {steps.stepsItems.map((item, idx) => (
-                        <li aria-current={steps.currentStep == idx + 1 ? "step" : false} className="flex-1 last:flex-none flex gap-x-2 md:items-center">
-                            <div className="flex items-center flex-col gap-x-2">
-                                <div className={`w-8 h-8 rounded-full border-2 flex-none flex items-center justify-center ${steps.currentStep > idx + 1 ? "bg-indigo-600 border-indigo-600" : "" || steps.currentStep == idx + 1 ? "border-indigo-600" : ""}`}>
-                                    <span className={` ${steps.currentStep > idx + 1 ? "hidden" : "" || steps.currentStep == idx + 1 ? "text-indigo-600" : ""}`}>
-                                        {idx + 1}
-                                    </span>
-                                    {
-                                        steps.currentStep > idx + 1 ? (
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-white">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                                            </svg>
-                                        ) : ""
-                                    }
-                                </div>
-                                <hr className={`h-12 border md:hidden ${idx + 1 == steps.stepsItems.length ? "hidden" : "" || steps.currentStep > idx + 1 ? "border-indigo-600" : ""}`} />
-                            </div>
-                            <div className="h-8 flex items-center md:h-auto">
-                                <h3 className={`text-sm ${steps.currentStep == idx + 1 ? "text-indigo-600" : ""}`}>
-                                    {item}
-                                </h3>
-                            </div>
-                            <hr className={`hidden mr-2 w-full border md:block ${idx + 1 == steps.stepsItems.length ? "hidden" : "" || steps.currentStep > idx + 1 ? "border-indigo-600" : ""}`} />
-                        </li>
-                    ))}
-                </ul>
+          <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="grid max-w-2xl mx-auto">
+              <div className="flex">
+                <div className="flex flex-col items-center mr-6">
+                  <div className="w-px h-10 opacity-0 sm:h-full" />
+                  <div>
+                    <div className="flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full">
+                      1
+                    </div>
+                  </div>
+                  <div className="w-px h-full bg-gray-300" />
+                </div>
+                <div className="flex flex-col pb-6 sm:items-center sm:flex-row sm:pb-0">
+                  <div className="sm:mr-5">
+                    <div className="flex items-center justify-center w-16 h-16 my-3 rounded-full bg-indigo-50 sm:w-24 sm:h-24">
+                      <svg
+                        className="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+                        stroke="currentColor"
+                        viewBox="0 0 52 52"
+                      >
+                        <polygon
+                          strokeWidth="3"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          fill="none"
+                          points="29 13 14 29 25 29 23 39 38 23 27 23"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <div>
+                    <p className="text-xl font-semibold sm:text-base">
+                      Read the recipe
+                    </p>
+                    <p className="text-sm text-gray-700">
+                      All recipes are written using certain conventions, which define
+                      the characteristics of common ingredients. The rules vary from
+                      place to place.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex">
+                <div className="flex flex-col items-center mr-6">
+                  <div className="w-px h-10 bg-gray-300 sm:h-full" />
+                  <div>
+                    <div className="flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full">
+                      2
+                    </div>
+                  </div>
+                  <div className="w-px h-full bg-gray-300" />
+                </div>
+                <div className="flex flex-col pb-6 sm:items-center sm:flex-row sm:pb-0">
+                  <div className="sm:mr-5">
+                    <div className="flex items-center justify-center w-16 h-16 my-3 rounded-full bg-indigo-50 sm:w-24 sm:h-24">
+                      <svg
+                        className="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+                        stroke="currentColor"
+                        viewBox="0 0 52 52"
+                      >
+                        <polygon
+                          strokeWidth="3"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          fill="none"
+                          points="29 13 14 29 25 29 23 39 38 23 27 23"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <div>
+                    <p className="text-xl font-semibold sm:text-base">Heart attack</p>
+                    <p className="text-sm text-gray-700">
+                      A flower in my garden, a mystery in my panties. Heart attack
+                      never stopped old Big Bear. I didn't even know we were calling
+                      him Big Bear.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex">
+                <div className="flex flex-col items-center mr-6">
+                  <div className="w-px h-10 bg-gray-300 sm:h-full" />
+                  <div>
+                    <div className="flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full">
+                      3
+                    </div>
+                  </div>
+                  <div className="w-px h-full opacity-0" />
+                </div>
+                <div className="flex flex-col pb-6 sm:items-center sm:flex-row sm:pb-0">
+                  <div className="sm:mr-5">
+                    <div className="flex items-center justify-center w-16 h-16 my-3 rounded-full bg-indigo-50 sm:w-24 sm:h-24">
+                      <svg
+                        className="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+                        stroke="currentColor"
+                        viewBox="0 0 52 52"
+                      >
+                        <polygon
+                          strokeWidth="3"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          fill="none"
+                          points="29 13 14 29 25 29 23 39 38 23 27 23"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <div>
+                    <p className="text-xl font-semibold sm:text-base">Never stop</p>
+                    <p className="text-sm text-gray-700">
+                      The first mate and his Skipper too will do their very best to
+                      make the others comfortable in their tropic island nest. Michael
+                      Knight a young loner.
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-        )
+          </div>
+        );
+    
     }
 
 
