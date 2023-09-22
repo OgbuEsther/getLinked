32be74bc96f3776648cb432068958a6dc6e000e6
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-09-22 20:55:36 +0100

    blur

--- src/model/pop-up/pop-up.tsx
+++ src/model/pop-up/pop-up.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import img from "./img1-removebg-preview.png";
-
+//
 const PopUp: React.FC = () => {
   return (
-    <div className="absolute top-0 w-screen h-screen bg-[#160e29ea] flex justify-center items-center">
+    <div className="absolute top-0 w-full h-screen bg-[#160e29ea] flex justify-center items-center backdrop-blur-md z-50">
       <div className="w-[300px] md:w-[400px] py-6 md:h-[440px] h-fit border-[1px] border-[#8d26ae] flex justify-center items-center">
         <div className="w-[88%] h-[86%] ">
           <div className="w-full h-[200px]">
