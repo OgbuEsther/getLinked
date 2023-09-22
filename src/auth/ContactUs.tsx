4ff8b4a72b2ad88203d739db7e9ac3a9b3972beb
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-09-22 10:54:39 +0100

    contact us

--- src/auth/ContactUs.tsx
+++ src/auth/ContactUs.tsx
@@ -10,13 +10,15 @@ const ContactUs = () => {
         <main className="w-[100%] flex">
             <div className="relative flex-1 hidden items-center justify-center h-screen bg-gray-900 lg:flex">
                 <div className="relative z-10 w-full max-w-md">
-                    <img src="https://floatui.com/logo-dark.svg" width={150} />
+                <span className="ml-2 text-xl font-bold tracking-wide text-gray-100">
+              get<span className='text-[#D434FE]'>Linked</span>
+            </span>
                     <div className=" mt-16 space-y-3">
                         <h3 className="text-white text-3xl font-bold">Start growing your business quickly</h3>
                         <p className="text-gray-300">
                             Create an account and get access to all features for 30-days, No credit card required.
                         </p>
-                        <div className="flex items-center -space-x-2 overflow-hidden">
+                        {/* <div className="flex items-center -space-x-2 overflow-hidden">
                             <img src="https://randomuser.me/api/portraits/women/79.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
                             <img src="https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
                             <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=a72ca28288878f8404a795f39642a46f" className="w-10 h-10 rounded-full border-2 border-white" />
@@ -25,13 +27,13 @@ const ContactUs = () => {
                             <p className="text-sm text-gray-400 font-medium translate-x-5">
                                 Join 5.000+ users
                             </p>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
                 <div
                     className="absolute inset-0 my-auto h-[500px]"
                     style={{
-                        background: "linear-gradient(152.92deg, rgba(192, 132, 252, 0.2) 4.54%, rgba(232, 121, 249, 0.26) 34.2%, rgba(192, 132, 252, 0.1) 77.55%)", filter: "blur(118px)"
+                        background: "linear-gradient(#150E28, #150E28, #2d2150", filter: "blur(118px)"
                     }}
                 >
 
