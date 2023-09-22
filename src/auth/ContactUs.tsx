8022bbcc05037d40ee8ed9bd91c7ca9e51b14e10
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-09-22 20:31:25 +0100

    pop up done

--- src/auth/ContactUs.tsx
+++ src/auth/ContactUs.tsx
@@ -11,6 +11,8 @@ const ContactUs = () => {
   const [first_name, setFirst_name] = useState("");
   const [message, setMessage] = useState("");
 
+  const [showPopUp, setShowPopUp] = useState(false);
+
   const sendMessage = async (e: any) => {
     e.preventDefault();
     await axios
@@ -26,14 +28,15 @@ const ContactUs = () => {
       )
       .then((res) => {
         console.log(res?.data);
-        <PopUp />;
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "mail sent successfully ",
-          showConfirmButton: false,
-          timer: 2500,
-        });
+        setShowPopUp(true);
+
+        // Swal.fire({
+        //   position: "center",
+        //   icon: "success",
+        //   title: "mail sent successfully ",
+        //   showConfirmButton: false,
+        //   timer: 2500,
+        // });
       })
       .catch((err) => {
         console.log(err);
@@ -41,128 +44,133 @@ const ContactUs = () => {
   };
 
   return (
-    <main className="w-[100%] flex bg-[#150E28] items-center">
-      <div className="relative flex-1 hidden items-center justify-center h-screen  lg:flex">
-        <div className="relative z-10 w-full max-w-md">
-          {/* <span className="ml-2 text-xl font-bold tracking-wide text-gray-100">
+    <div>
+      {showPopUp && <PopUp />}
+      <main className="w-[100%] flex bg-[#150E28] items-center">
+        <div className="relative flex-1 hidden items-center justify-center h-screen  lg:flex">
+          <div className="relative z-10 w-full max-w-md">
+            {/* <span className="ml-2 text-xl font-bold tracking-wide text-gray-100">
             get<span className="text-[#D434FE]">Linked</span>
           </span> */}
-          <div className=" mt-16 space-y-3">
-            <h2 className="text-[#D434FE] text-3xl font-bold">Get in touch</h2>
-            <p className="text-white font-semibold">
-              Contact <br />
-              information
-            </p>
-            <br />
-            <p className="text-white font-semibold">
-              27 , Alara Street <br />
-              Yaba 100012 <br />
-              Lagos State
-            </p>
-            <br />
-            <p className="text-white font-semibold">Call Us : 07067981819</p>
-            <br />
-            <p className="text-white font-semibold">
-              we are open from Monday-Friday <br /> 08:00am - 05:00pm
-            </p>
-            <br />
-            <br />
-            <p className="text-[#D434FE] font-semibold">Share On</p>
-          </div>
-        </div>
-        <div className="absolute inset-0 my-auto h-[500px]"></div>
-      </div>
-      <div className="flex-1 flex items-center justify-center sm:h-screen lg:h-[80vh] shadow-lg">
-        <div className="w-full max-w-md space-y-8 px-4 p-[30px] bg-transparent text-gray-600 sm:px-0">
-          <div className="">
-            <div className="mt-5 space-y-2">
-              <h3 className="text-[#D434FE] text-2xl font-bold sm:text-3xl">
-                Questions or need assistance ? Let us know about it{" "}
-              </h3>
+            <div className=" mt-16 space-y-3">
+              <h2 className="text-[#D434FE] text-3xl font-bold">
+                Get in touch
+              </h2>
+              <p className="text-white font-semibold">
+                Contact <br />
+                information
+              </p>
+              <br />
+              <p className="text-white font-semibold">
+                27 , Alara Street <br />
+                Yaba 100012 <br />
+                Lagos State
+              </p>
+              <br />
+              <p className="text-white font-semibold">Call Us : 07067981819</p>
+              <br />
+              <p className="text-white font-semibold">
+                we are open from Monday-Friday <br /> 08:00am - 05:00pm
+              </p>
+              <br />
+              <br />
+              <p className="text-[#D434FE] font-semibold">Share On</p>
             </div>
           </div>
-          <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
-            <div>
-              <label className="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
-                <input
-                  onChange={(e) => {
-                    setFirst_name(e.target.value);
-                  }}
-                  type="text"
-                  id="first_name"
-                  className="peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 w-[200px] h-[50px] text-white "
-                  placeholder="first_name"
-                />
-
-                <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-transparent p-0.5 text-xs text-white transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
-                  First Name
-                </span>
-              </label>
-            </div>
-            <div>
-              <label className="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
-                <input
-                  onChange={(e: any) => {
-                    setPhone_number(e.target.value);
-                  }}
-                  type="text"
-                  id="phone_number"
-                  className="peer text-white border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 w-[200px] h-[50px] "
-                  placeholder="phone_number"
-                />
-
-                <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-transparent p-0.5 text-xs text-white transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
-                  Phone Number
-                </span>
-              </label>
-            </div>
-            <div>
-              <label className="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
-                <input
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                  type="text"
-                  id="Mail"
-                  className="peer text-white border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 w-[200px] h-[50px] "
-                  placeholder="Mail"
-                />
-
-                <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-transparent p-0.5 text-xs text-white transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
-                  Mail
-                </span>
-              </label>
-            </div>
-            <div>
-              <label className="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
-                <input
-                  onChange={(e) => {
-                    setMessage(e.target.value);
-                  }}
-                  type="text"
-                  id="Message"
-                  className="peer text-white border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 w-[200px] h-[100px] "
-                  placeholder="Message"
-                />
-
-                <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-transparent p-0.5 text-xs text-white transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
-                  Message
-                </span>
-              </label>
-            </div>
-
-            <div className="mb-10 mt-[50px]  sm:text-center sm:mt-[30px] md:mb-16 lg:mb-20">
-              <button
-                onClick={sendMessage}
-                className="inline-flex items-center p-[20px] justify-center w-auto h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 bg-gradient-to-r from-[#D434FE] to-[#903AFF]  hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+          <div className="absolute inset-0 my-auto h-[500px]"></div>
         </div>
-      </div>
-    </main>
+        <div className="flex-1 flex items-center justify-center sm:h-screen lg:h-[80vh] shadow-lg">
+          <div className="w-full max-w-md space-y-8 px-4 p-[30px] bg-transparent text-gray-600 sm:px-0">
+            <div className="">
+              <div className="mt-5 space-y-2">
+                <h3 className="text-[#D434FE] text-2xl font-bold sm:text-3xl">
+                  Questions or need assistance ? Let us know about it{" "}
+                </h3>
+              </div>
+            </div>
+            <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
+              <div>
+                <label className="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
+                  <input
+                    onChange={(e) => {
+                      setFirst_name(e.target.value);
+                    }}
+                    type="text"
+                    id="first_name"
+                    className="peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 w-[200px] h-[50px] text-white "
+                    placeholder="first_name"
+                  />
+
+                  <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-transparent p-0.5 text-xs text-white transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
+                    First Name
+                  </span>
+                </label>
+              </div>
+              <div>
+                <label className="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
+                  <input
+                    onChange={(e: any) => {
+                      setPhone_number(e.target.value);
+                    }}
+                    type="text"
+                    id="phone_number"
+                    className="peer text-white border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 w-[200px] h-[50px] "
+                    placeholder="phone_number"
+                  />
+
+                  <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-transparent p-0.5 text-xs text-white transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
+                    Phone Number
+                  </span>
+                </label>
+              </div>
+              <div>
+                <label className="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
+                  <input
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    type="text"
+                    id="Mail"
+                    className="peer text-white border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 w-[200px] h-[50px] "
+                    placeholder="Mail"
+                  />
+
+                  <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-transparent p-0.5 text-xs text-white transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
+                    Mail
+                  </span>
+                </label>
+              </div>
+              <div>
+                <label className="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
+                  <input
+                    onChange={(e) => {
+                      setMessage(e.target.value);
+                    }}
+                    type="text"
+                    id="Message"
+                    className="peer text-white border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 w-[200px] h-[100px] "
+                    placeholder="Message"
+                  />
+
+                  <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-transparent p-0.5 text-xs text-white transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
+                    Message
+                  </span>
+                </label>
+              </div>
+
+              <div className="mb-10 mt-[50px]  sm:text-center sm:mt-[30px] md:mb-16 lg:mb-20">
+                <button
+                  onClick={sendMessage}
+                  className="inline-flex items-center p-[20px] justify-center w-auto h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 bg-gradient-to-r from-[#D434FE] to-[#903AFF]  hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 };
 
