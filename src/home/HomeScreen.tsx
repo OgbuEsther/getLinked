d5d138d32d0d8672f810d76db331ec95682269d0
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-09-23 15:10:12 +0100

    testing

--- src/home/HomeScreen.tsx
+++ src/home/HomeScreen.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import Header from "./Header";
 import Hero from "./Hero";
-import Content from "./Content";
 import Content2 from "./Content2";
 import FAQs from "./FAQs";
 import TimeLine from "./TimeLine";
@@ -12,19 +10,24 @@ import Prizes from "./Prizes";
 import PrivacyPolicy from "./PrivacyPolicy";
 import big from "../assets/bigbulb.png";
 import girl from "../assets/girl.png";
+
 const HomeScreen = () => {
   return (
     <div className=" bg-[#150E28] ">
       <Header />
       <Hero />
-      <Content2 order="" img={big} />
-      <Content2 order="order-2 max-md:order-none" img={girl} />
+      <Content2 order="" img={big} visibility="hidden" />
+      <Content2 order="order-2 max-md:order-none" img={girl} visibility="" />
       <Judge />
       <FAQs />
+
       <TimeLine />
+      <hr />
       <Prizes />
       <Partners />
+
       <PrivacyPolicy />
+
       <Footer />
     </div>
   );
