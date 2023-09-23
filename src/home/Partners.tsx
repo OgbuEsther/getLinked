017749410c512b2c7f56e2bd76530f33cb65ad91
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-09-23 20:35:35 +0100

    DONE

--- src/home/Partners.tsx
+++ src/home/Partners.tsx
@@ -1,14 +1,10 @@
-import liberty from "../assets/Liberty.png";
-import win from "../assets/Liberty(2).png";
-import imf from "../assets/wisper.png";
-import winwise from "../assets/Winwise.png";
-import pb from "../assets/Paybox.png";
-import vp from "../assets/VizualPlus.png";
+import partners from "../assets/Partner_and_Sponsors.png";
+import flare from "../assets/Purple-Lens-Flare.svg";
 
 const Partners = () => {
   return (
-    <div className=" pt-36 pb-20 bg-[#150e29] border-b border-slate-800">
-      <div className="w-[90%] m-auto ">
+    <div className=" pt-36 pb-20 bg-[#1dod3a] border-b border-slate-800">
+      <div className="w-[80%] m-auto ">
         <h1 className="text-3xl tracking-tighter leading-1 text-white text-center font-extrabold max-md:text-center max-md:text-2xl">
           Partners and Sponsors
         </h1>
@@ -17,33 +13,20 @@ const Partners = () => {
           companies as its partners and sponsors
         </p>
         <div className="w-full grid place-items-center py-10 border mt-6 border-[var(--accent-color)] rounded">
-          <div className="w-[50%] grid py-10 grid-cols-3 gap-4 place-items-center max-md:w-[80%]">
-            <div className="">
-              <img
-                src={liberty}
-                alt=""
-                className="object-contain w-36 max-md:w-20"
-              />
+          <div className=" relative w-[50%] grid py-10  place-items-center max-md:w-[80%]">
+            <div>
+              <img src={partners} alt="" className="object-contain " />
             </div>
-            <div className="">
-              <img src={win} alt="" className="object-contain w-30" />
-            </div>
-            <div className="">
-              <img src={winwise} alt="" className="object-contain w-36" />
-            </div>
-            <div className="">
-              <img
-                src={imf}
-                alt=""
-                className="object-contain w-20 max-md:w-12"
-              />
-            </div>
-            <div className="">
-              <img src={pb} alt="" className="object-contain " />
-            </div>
-            <div className="">
-              <img src={vp} alt="" className="object-contain w-40" />
-            </div>
+            <img
+              src={flare}
+              style={{
+                position: "absolute",
+                top: -300,
+                left: -400,
+                height: "90vh",
+              }}
+              className=""
+            />
           </div>
         </div>
       </div>
